e94a282fd22ec3ea82e867053f3b86ce11bf98e2
Mark Marlow <theMarlzy@gmail.com>
2022-01-27 10:57:49 +1100

    updated smh parser... seems pretty good

--- smh.py
+++ smh.py
@@ -3,9 +3,10 @@ import logging
 import os
 import sys
 from typing import List, Tuple, Dict
-from itsdangerous import json
+import json
 import requests
 import scrapy
+from scrapy.shell import inspect_response
 import re
 
 
@@ -14,10 +15,11 @@ class SMHSpider(scrapy.Spider):
 
     def start_requests(self):
         url = "smh.com.au"
-        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(message)s",)
-        start_date = datetime(2021,1,1)
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+                            format="%(asctime)s [%(levelname)s] %(message)s",)
+        start_date = datetime(2021, 1, 1)
         end_date = datetime.now()
-        snapshots = all_snapshots(url, start_date, end_date)[:10]
+        snapshots = all_snapshots(url, start_date, end_date)
         for snapshot in snapshots:
             ts_str = snapshot[1]
             file_output = f"output/smh_{ts_str}.html"
@@ -26,46 +28,52 @@ class SMHSpider(scrapy.Spider):
             url = snapshot[2]
             wm_url = f"https://web.archive.org/web/{ts_str}id_/{url}"
             yield scrapy.Request(url=wm_url, callback=self.parse, meta={"snapshot": snapshot})
-    
+
     def download(self, response):
         ts_str = response.meta['snapshot'][1]
         file_output = f"output/smh_{ts_str}.html"
         logging.debug(f"Saving file: {file_output}")
         if not os.path.exists("output"):
             os.mkdir("output")
-        print(response.body, file=open(file_output,"w",encoding="utf8"))
-    
+        print(response.body, file=open(file_output, "w", encoding="utf8"))
+
     def parse(self, response):
         ts = response.meta['snapshot'][1]
-        raw = re.search("INITIAL_STATE = (JSON.parse\()?(.+?)(\))?;?<",str(response.body))
+        raw = re.search(
+            "INITIAL_STATE = (JSON.parse\()?(.+?)(\))?;?<", str(response.body))
         if raw is None:
             logging.info(f"No JSON in {ts}")
             yield {"snapshot": ts}
-        
+
         raw = raw.group(2)
-        j = json.loads(json.loads(raw.replace("\\\\(.)", r"\1")))
-        content_unit_groups = [ug for ug in j['page']['/']['index']['contentUnitGroups'] if ug['label'] in ["News well","Everything else"]]
-        
+        j = json.loads(re.sub("\\\\(.)", r"\1", raw))
+        if type(j) == str:
+            j = json.loads(j)
+
+        content_unit_groups = [ug for ug in j['page']['/']['index']
+                               ['contentUnitGroups'] if ug['label'] in ["News well", "Everything else"]]
+
         for content_unit_group in content_unit_groups:
             cug_type = content_unit_group['label']
             units = content_unit_group['contentUnits']
             for content_unit in units:
                 content_unit_id = content_unit['id']
-                for asset in content_unit['assets']:
+                assets = content_unit.get("assets",[])
+                for asset in assets:
                     if asset['assetType'] != 'article':
                         continue
                     
-                    about = asset['asset']['about']
-                    headline = asset['asset']['headlines']['headline']
-                    byline = asset['asset']['byline']
-                    word_count = asset['asset']['wordCount']
-                    cat = asset['category']['name']
+                    about = asset.get('asset', {}).get('about')
+                    headline = asset.get('asset', {}).get(
+                        'headlines', {}).get('headline')
+                    byline = asset.get('asset', {}).get('byline')
+                    word_count = asset.get('asset', {}).get('wordCount')
+                    cat = asset.get('category',{}).get('name')
                     id = asset['id']
-                    first_published = asset['dates']['firstPublished']
-                    modified = asset['dates']['modified']
-                    published = asset['dates']['published']
-
-                    yield dict(
+                    first_published = asset.get('dates',{}).get('firstPublished')
+                    modified = asset.get('dates',{}).get('modified')
+                    published = asset.get('dates',{}).get('published')
+                    output = dict(
                         about=about,
                         headline=headline,
                         byline=byline,
@@ -78,12 +86,10 @@ class SMHSpider(scrapy.Spider):
                         cug_type=cug_type,
                         snapshot=ts,
                         content_unit_id=content_unit_id
-                    ) 
+                    )
 
-        
+                    yield output
 
-
-    
 
 def all_snapshots(url: str, start_date: datetime, end_date: datetime) -> List[List[str]]:
     ts_start = start_date.strftime("%Y%m%d%H%M%S")
@@ -92,9 +98,9 @@ def all_snapshots(url: str, start_date: datetime, end_date: datetime) -> List[Li
     resp = requests.get(cdx_url)
     return parse_cdx(resp.text)
 
+
 def parse_cdx(txt: str) -> List[List[str]]:
     lines: List[str] = txt.splitlines(keepends=False)
     split_lines = [line.split(" ") for line in lines]
     filtered = [line for line in split_lines if line[3] == "text/html"]
     return filtered
-
